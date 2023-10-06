6d8d8c0e61baf0fbd55ae2f19194bfe472adbda6
OMI786I <sohrawardy1998@gmail.com>
2023-10-06 12:34:18 +0600

    Custom Tailwind class and font setup

--- Dragon_News/src/Pages/Home.jsx
+++ Dragon_News/src/Pages/Home.jsx
@@ -1,5 +1,14 @@
+import Header from "./Shared/Header";
+import Navbar from "./Shared/Navbar";
+
 const Home = () => {
-  return <div>This i home</div>;
+  return (
+    <div>
+      <Header></Header>
+      <Navbar></Navbar>
+      <h2 className="text-3xl font-poppins font-bold"> This is Home</h2>
+    </div>
+  );
 };
 
 export default Home;
---
+++ Dragon_News/src/Pages/Shared/RightSideNav/RighSideNav.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RighSideNav = () => {
+  return (
+    <div>
+      <h2>right side</h2>
+    </div>
+  );
+};
+
+export default RighSideNav;
