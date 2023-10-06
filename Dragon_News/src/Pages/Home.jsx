0fdd5946bcc80963d4c04f145784c61192694b49
OMI786I <sohrawardy1998@gmail.com>
2023-10-06 12:10:04 +0600

    router setup

---
+++ Dragon_News/src/Pages/Home.jsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div>This i home</div>;
+};
+
+export default Home;
--- Dragon_News/src/main.jsx
+++ Dragon_News/src/main.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./layout/Root";
+import Home from "./Pages/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root></Root>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
   },
 ]);
 
