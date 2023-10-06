d254569963327d42c210c981c751022644e0f89e
OMI786I <sohrawardy1998@gmail.com>
2023-10-06 21:34:29 +0600

    Left side navigation

--- Dragon_News/src/Pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ Dragon_News/src/Pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const LeftSideNav = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("categories.json")
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
+  }, []);
   return (
-    <div>
-      <h2>LEft side</h2>
+    <div className="space-y-4">
+      <h2>All Categories</h2>
+      {categories.map((category) => (
+        <Link
+          className="block ml-4 text-xl font-semibold"
+          key={category.id}
+          to={`/category/${category.id}`}
+        >
+          {category.name}
+        </Link>
+      ))}
     </div>
   );
 };
